e828ac7e91fd00b2cc791d729639894009b6062c
prefabcode <prefabcode@gmail.com>
2024-08-10 22:48:10 -0400

    Initial Commit

---
+++ MemPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemPulse
+//
+//  Created by Leonid Pinskiy on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemPulse/MemPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemPulseApp.swift
+//  MemPulse
+//
+//  Created by Leonid Pinskiy on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MemPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
